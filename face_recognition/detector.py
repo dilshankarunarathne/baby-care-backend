1d197e1ed5269edba75ac2dae262bb353a7d2aa4
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2024-01-29 23:27:18 +0530

    Update detector.py

--- face_recognition/detector.py
+++ face_recognition/detector.py
@@ -16,19 +16,14 @@ BOUNDING_BOX_COLOR = "blue"
 TEXT_COLOR = "white"
 
 
-def recognize_faces(
-        base64_image_data, model: str = "hog", encodings_location: Path = DEFAULT_ENCODINGS_PATH
-) -> list:
-    """Recognizes faces in a base64-encoded image string and returns a list of detected names."""
+def recognize_faces(frame, model: str = "hog", encodings_location: Path = DEFAULT_ENCODINGS_PATH) -> list:
+    """Recognizes faces in an image frame and returns a list of detected names."""
 
     with encodings_location.open(mode="rb") as f:
         loaded_encodings = pickle.load(f)
 
-    image_data = base64.b64decode(base64_image_data)
-    input_image = face_recognition.load_image_file(io.BytesIO(image_data))
-
-    input_face_locations = face_recognition.face_locations(input_image, model=model)
-    input_face_encodings = face_recognition.face_encodings(input_image, input_face_locations)
+    input_face_locations = face_recognition.face_locations(frame, model=model)
+    input_face_encodings = face_recognition.face_encodings(frame, input_face_locations)
 
     names = []
     for unknown_encoding in input_face_encodings:
