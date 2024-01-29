21cb4a197255b52ad07457e7b9f4c83af151386e
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2024-01-29 22:28:57 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from fastapi import FastAPI
 
-from routes import auth
+from routes import auth, image_route
 
 app = FastAPI()
 
 app.include_router(auth.router)
-
+app.include_router(image_route.router)
