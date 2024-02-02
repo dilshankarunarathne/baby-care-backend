f8efe7556430614f9ea97ebac8be75d12d21538b
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2024-02-03 02:11:15 +0530

    Update main.py

--- posture/main.py
+++ posture/main.py
@@ -5,3 +5,7 @@ import numpy as np
 # Load the model
 model_path = "keras_model.h5"
 model = load_model(model_path, compile=False)
+
+# Load the labels
+labels_path = "labels.txt"
+class_names = open(labels_path, "r").readlines()
