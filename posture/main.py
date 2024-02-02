58079ad8ce383f27dcbc93276b998ad2f21ae36b
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2024-02-03 02:11:09 +0530

    Update main.py

--- posture/main.py
+++ posture/main.py
@@ -2,4 +2,6 @@ from keras.models import load_model
 from PIL import Image, ImageOps
 import numpy as np
 
-
+# Load the model
+model_path = "keras_model.h5"
+model = load_model(model_path, compile=False)
