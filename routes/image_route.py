9650fb76373caf30133a7f8da99915fb7b75066e
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2024-02-03 22:50:31 +0530

    Update image_route.py

--- routes/image_route.py
+++ routes/image_route.py
@@ -83,7 +83,6 @@ async def check_baby_image_endpoint(
     # estimate sleep position
     pose, pose_confidence = analyze_posture(frame)
 
-
     if class_name == "Unknown":
         return {"message": "Baby not detected"}
 
