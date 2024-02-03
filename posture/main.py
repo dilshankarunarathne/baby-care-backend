4d7fa1e27f9739b7b97b09493f193e3a92379f96
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2024-02-03 21:36:50 +0530

    Update main.py

--- posture/main.py
+++ posture/main.py
@@ -4,11 +4,11 @@ from PIL import Image, ImageOps
 import numpy as np
 
 # Load the model
-model_path = "keras_model.h5"
+model_path = "posture/keras_model.h5"
 model = load_model(model_path, compile=False)
 
 # Load the labels
-labels_path = "labels.txt"
+labels_path = "posture/labels.txt"
 class_names = open(labels_path, "r").readlines()
 
 
