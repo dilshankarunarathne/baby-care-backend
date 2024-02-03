279104cd35c04da1b5946e77a8292180f019fcf2
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2024-02-03 21:55:24 +0530

    Update tm_main.py

--- face_rec/tm_main.py
+++ face_rec/tm_main.py
@@ -6,10 +6,10 @@ import numpy as np
 np.set_printoptions(suppress=True)
 
 # Load the model
-model = load_model("keras_Model.h5", compile=False)
+model = load_model("face_rec/keras_Model.h5", compile=False)
 
 # Load the labels
-class_names = open("labels.txt", "r").readlines()
+class_names = open("face_rec/labels.txt", "r").readlines()
 
 def analyze_image(img_data):
     # Convert the numpy array back to an image
